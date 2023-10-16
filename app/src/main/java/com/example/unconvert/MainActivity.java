f17fa4de1331d87de0bdbeb8ea16b7b91ea314cd
monotoshghosh <monotoshghosh49@gmail.com>
2023-10-16 15:35:51 +0530

    Done Till Image Adding And Background

---
+++ app/src/main/java/com/example/unconvert/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.unconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //
+    private EditText editText;
+    private TextView textView;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+        button=findViewById(R.id.button);
+        textView=findViewById(R.id.textView2);
+        editText=findViewById(R.id.editTextText);
+        //
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+
+                String s= editText.getText().toString();
+                int Kg=Integer.parseInt(s);
+                double pound=2.205*Kg;
+                textView.setText("The Value is "+pound);
+
+            }
+        });
+
+
+
+    }
+}
